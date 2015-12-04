b04a38f03a56168f45e5d722e2463538e60e673e
Sergio Arcos <sarcos@moneytree.jp>
2015-12-04 13:53:32 +0900

    first version

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# mocha-lambda-reporter
-Mocha Lambda Reporter
+Useful for test your live AWS lambdas and obtain how long they spent depending of the memory and the number of times
+
+INSTALL
+=======
+
+    npm install --save-dev mocha-lambda-reporter
+
+
+FEATURES
+========
+
+Creates new Reporter called `mocha-lambda-ui` based on BDD. Example:
+
+    mocha -t 30000 -R mocha-lambda-reporter -u mocha-lambda-ui -c tests/live/**/*.js;
+
+It should be used with mocha-lambda-reporter to see the invokation time
+
+
+EXAMPLE
+=======
+
+/tests/live/module-function.js
+
+    describe('module-function', function () {
+
+      lambdaConfig(require('./config.json')['module-function']);
+
+      lt('Check response', function () {
+        var event = {}; // required
+        var context = {}; // optional
+        return this.test.lambda(event, context).then(function (actual) {
+          var expected = '';
+          expect(actual).be.equal(expected);
+        });
+      });
+    });
+
+/tests/live/config.json
+
+    {
+      "module-function": {
+        "functionName": "dev-ProjectName-l-lModuleFunction-XXXXXXXX",
+        "count": 5,
+        "memoryArray": [128, 512, 1024, 1536]
+      }
+    }
---
+++ index.js
@@ -0,0 +1,127 @@
+/**
+ * Module dependencies.
+ */
+
+var Base = require('mocha/lib/reporters/base');
+var inherits = require('mocha/lib/utils').inherits;
+var color = Base.color;
+var cursor = Base.cursor;
+var Table = require('cli-table');
+
+/**
+ * Expose `Spec`.
+ */
+
+exports = module.exports = Spec;
+
+/**
+ * Initialize a new `Spec` test reporter.
+ *
+ * @api public
+ * @param {Runner} runner
+ */
+function Spec(runner) {
+  Base.call(this, runner);
+
+  var self = this;
+  var indents = 0;
+  var n = 0;
+
+  function indent() {
+    return Array(indents).join('  ');
+  }
+
+  runner.on('start', function() {
+    console.log();
+  });
+
+  runner.on('suite', function(suite) {
+    mySuite = suite;
+    ++indents;
+    console.log(color('suite', '%s%s'), indent(),
+      (suite.myConfig && suite.myConfig.functionName) ? 'λ(' + suite.title + ') ' : suite.title);
+  });
+
+  runner.on('suite end', function() {
+    --indents;
+    if (indents === 1) {
+      printTable();
+      console.log();
+    }
+  });
+
+  runner.on('pending', function(test) {
+    var fmt = indent() + color('pending', '  - %s');
+    console.log(fmt, test.title);
+  });
+
+  var mySuite;
+  var myTable;
+  var old_test_title;
+
+  function printTable() {
+    if (myTable) {
+      // colors ??
+
+      console.log(myTable.toString());
+      myTable = null;
+    }
+  }
+
+  runner.on('pass', function(test) {
+    if (!test.lambdaInvokeTime) {
+      var fmt;
+      if (test.speed === 'fast') {
+        fmt = indent()
+          + color('checkmark', '  ' + Base.symbols.ok)
+          + color('pass', ' %s');
+        cursor.CR();
+        console.log(fmt, test.title);
+      } else {
+        fmt = indent()
+          + color('checkmark', '  ' + Base.symbols.ok)
+          + color('pass', ' %s')
+          + color(test.speed, ' (%dms)');
+        cursor.CR();
+        console.log(fmt, test.title, test.duration);
+      }
+    } else {
+      if (old_test_title != test.title) {
+        printTable();
+        cursor.CR();
+        console.log(indent(), test.title);
+
+        myTable = new Table({
+          head: [color('checkmark', '' + Base.symbols.ok)].concat(mySuite.myConfig.memoryArray),
+          style: {head: ['bold'], border: ['grey']}
+        });
+
+        for (var i = 0; i < mySuite.myConfig.count; i++) {
+          var arr = [i+1];
+          for (var j = 0; j < mySuite.myConfig.memoryArray.length; j++) {
+            arr.push('');
+          }
+          myTable.push(arr);
+        }
+      }
+
+      myTable[test.lambdaNumberId][test.lambdaMemoryId + 1] = test.lambdaInvokeTime;
+    }
+
+    old_test_title = test.title;
+  });
+
+  runner.on('fail', function(test) {
+    cursor.CR();
+    console.log(indent() + color('fail', '  %d) %s'), ++n, test.title);
+  });
+
+  runner.on('end', function() {
+    self.epilogue.bind(self);
+  });
+}
+
+/**
+ * Inherit from `Base.prototype`.
+ */
+inherits(Spec, Base);
